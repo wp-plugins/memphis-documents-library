db84abd64a750f14f7885cc959efca7aa9f5455a
bhaldie <bhaldie@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-28 13:49:50 +0000

    [ Release ][ Version 2.4.1 ] * fixed short code, not showing categories. * special character changes. * lots of bug fixes * optimization of code
    git-svn-id: https://plugins.svn.wordpress.org/memphis-documents-library/tags/2.4.1@922492 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ mdocs-batch-upload.php
@@ -0,0 +1,156 @@
+<?php
+function mdocs_batch_upload($current_cat) {
+	$do_zip = false;
+	//$mdocs = get_option('mdocs-list');
+	//$cats = get_option('mdocs-cats');
+	//var_dump($cats);
+	$do_complte = false;
+	if(isset($_FILES['mdocs-batch']) && $_FILES['mdocs-batch']['type'] != 'application/zip') {
+		?>
+		<div class="error">
+			<p><?php _e('Please upload a zip file.'); ?></p>
+		</div>
+		<?php
+	} elseif(isset($_FILES['mdocs-batch']) && $_FILES['mdocs-batch']['type'] == 'application/zip') {
+		if(!file_exists('/tmp/')) mkdir('/tmp/');
+		if(!file_exists('/tmp/mdocs/'))mkdir('/tmp/mdocs/');
+		$zip_result = mdocs_unzip($_FILES['mdocs-batch']['tmp_name'], '/tmp');
+		$do_zip = true;
+	} elseif (isset($_POST['mdocs-batch-complete'])) {
+		$do_complte = true;
+	}
+?>
+<h1><?php _e('Batch Library Upload'); ?></h1>
+<div class="error">
+	<h3>Warning</h3>
+	<p><?php _e('Batch Upload  is still in beta.  Be sure to backup your library before running this process.  If anything should go wrong, just import the backup using the"Overwrite Saved Variables" option, and then run the "File System Cleanup" process to revert to the original state.'); ?></p></div>
+<div class="updated">
+	<p><?php _e('Create a zip file of all the documents you want to upload.  You may name it whatever you want, naming doesn\'t matter.  Once you have created the file, simply upload it, then use the quick select form to place the files in the proper directory.  Once satisfied press the \'Complete\' button to finsh the process.'); ?></p>
+</div>
+<?php if($do_zip == false && $do_complte == false) { ?>
+<form class="mdocs-uploader-form" enctype="multipart/form-data" action="<?php echo get_site_url().'/wp-admin/admin.php?page='.$_REQUEST['page'].'&cat='.$_REQUEST['cat']; ?>" method="POST">
+	<input type="file" name="mdocs-batch" /><br/>
+	<input type="submit" class="button button-primary" value="<?php _e('Upload Zip File') ?>" /><br/>
+</form>
+<?php } elseif($do_zip) {
+	$cats = get_option('mdocs-cats');
+	$current_cat = key($cats);
+	?>
+	<form class="mdocs-uploader-form" enctype="multipart/form-data" action="<?php echo get_site_url().'/wp-admin/admin.php?page='.$_REQUEST['page'].'&cat='.$_REQUEST['cat']; ?>" method="POST">
+		<input type="hidden" name="mdocs-batch-complete" value="1" />
+		<input type="hidden" name="mdocs-type" value="mdocs-add" />
+		<?php
+		foreach($zip_result['file'] as $index => $zip_file) {
+			$file = explode('/',$zip_file);
+			if(count($file) == 1) $file = explode('\\',$zip_file);
+			$file = $file[count($file)-1];
+			$file = preg_replace('/[^A-Za-z0-9\-._]/', '', $file);
+			$file = str_replace(' ','', $file);
+			$filename = $file;
+			$file = explode('.',$file);
+			$file = $file[0];
+			?>
+			<div class="mdocs-batch-container">
+				<input type="hidden" name="mdocs[filename][<?php echo $index; ?>]" value="<?php echo $filename; ?>" />
+				<input type="hidden" name="mdocs[tmp-file][<?php echo $index; ?>]" value="<?php echo $zip_file; ?>" />
+				<label><?php _e('File Name'); ?>:
+					<input type="text" name="mdocs[name][<?php echo $index; ?>]" value="<?php echo $file; ?>"/>
+				</label>
+				<label><?php _e('Category'); ?>:
+					<select name="mdocs[cat][<?php echo $index; ?>]">
+						<?php
+							foreach( $cats as $select => $cat ){ 
+								
+								echo '<option  value="'.$cat['slug'].'" >'.$cat['name'].'</option>';
+							}
+						?>
+					</select>
+				</label>
+				<label>
+						<?php _e('Version'); ?>: 
+					<input type="text" name="mdocs[version][<?php echo $index; ?>]" value="1.0" />
+				</label>
+			</div>
+			<?php
+		}
+		?>
+		<br>
+		<input type="submit" class="button button-primary" value="<?php _e('Complete') ?>" /><br/>
+	</form>
+	<?php
+} elseif ($_POST['mdocs-batch-complete'] ) {
+	$file = array();
+	$current_user = wp_get_current_user();
+	$batch_log = '';
+	foreach($_POST['mdocs']['tmp-file'] as $index => $tmp) {
+		$valid_mime_type = false;
+		$file['name'] = $_POST['mdocs']['filename'][$index];
+		$result = wp_check_filetype($tmp);
+		$file['tmp_name'] = $tmp;
+		$file['error'] = 0;
+		if(file_exists($tmp)) $file['size'] = filesize($tmp);
+		$file['post_status'] = 'publish';
+		$file['post-status'] = 'publish';
+		//MDOCS FILE TYPE VERIFICATION	
+		$mimes = get_allowed_mime_types();
+		foreach ($mimes as $type => $mime) {
+		  if ($mime === $result['type']) {
+			$valid_mime_type = true;
+			break;
+		  }
+		}
+		$batch_log .= _('Processed File => ').$file['name']."<br>";
+		if($valid_mime_type) {
+			$upload = mdocs_process_file($file);
+			$mdocs = get_option('mdocs-list');
+			array_push($mdocs, array(
+				'id'=>(string)$upload['attachment_id'],
+				'parent'=>(string)$upload['parent_id'],
+				'filename'=>$upload['filename'],
+				'name'=>$upload['name'],
+				'desc'=>$upload['desc'],
+				'type'=>$result['type'],
+				'cat'=>$_POST['mdocs']['cat'][$index],
+				'owner'=>$current_user->user_login,
+				'size'=>(string)$file['size'],
+				'modified'=>(string)time(),
+				'version'=>(string)$_POST['mdocs']['version'][$index],
+				'show_social'=>(string)'on',
+				'non_members'=> (string)'on',
+				'file_status'=>(string)'public',
+				'post_status'=> (string)'publish',
+				'post_status_sys'=> (string)'publish',
+				'doc_preview'=>(string)'',
+				'downloads'=>(string)0,
+				'archived'=>array(),
+				'ratings'=>array(),
+				'rating'=>0
+			));
+			$mdocs = mdocs_array_sort($mdocs, 'name', SORT_ASC);
+			update_option('mdocs-list', $mdocs);
+			$batch_log .= _('Mime Type Allowed => ').$result['type']."<br>";
+			$batch_log .= _('File Uploaded with No Errors.')."<br><br>";
+		} else {
+			$batch_log .= __("Invalid Mime Type => ").$result['type']._(" Unable to process file.")."<br>";
+			$batch_log .= _('File Was Not Uploaded because an Error occured.')."<br><br>";
+		}
+		$file = array();
+	}
+	$batch_log .= _("Cleaning up tmp folder and files")."<br><br>";
+	$files = glob('/tmp/mdocs/*'); 
+	foreach($files as $file) if(is_file($file)) unlink($file);
+	if(file_exists('/tmp/mdocs')) @rmdir('/tmp/mdocs');
+	$batch_log .= _("Batch Process Complete.");
+	?>
+	<div class="updated">
+		<p><?php _e('The batch process has completed, below is a log of results:'); ?></p>
+		<p><?php echo $batch_log; ?></p>
+	</div>
+	<form class="mdocs-uploader-form" enctype="multipart/form-data" action="<?php echo get_site_url().'/wp-admin/admin.php?page='.$_REQUEST['page'].'&cat='.$_REQUEST['cat']; ?>" method="POST">
+		<input type="file" name="mdocs-batch" /><br/>
+		<input type="submit" class="button button-primary" value="<?php _e('Upload Zip File') ?>" /><br/>
+	</form>
+	<?php
+}
+}
+?>
--- mdocs-categories.php
+++ mdocs-categories.php
@@ -13,6 +13,7 @@ function mdocs_edit_cats() {
 						<th scope="col" class="manage-column column-name" ><?php _e('Category'); ?></th>
 						<th scope="col"  class="manage-column column-name" ><?php _e('Order'); ?></th>
 						<th scope="col"  class="manage-column column-name" ><?php _e('Remove'); ?></th>
+						<!--<th scope="col" class="manage-column column-name" ><?php _e('Add Category'); ?></th>-->
 					</tr>
 				</thead>
 				<tfoot>
@@ -20,30 +21,29 @@ function mdocs_edit_cats() {
 						<th scope="col" class="manage-column column-name" ><?php _e('Category'); ?></th>
 						<th scope="col" class="manage-column column-name" ><?php _e('Order'); ?></th>
 						<th scope="col" class="manage-column column-name" ><?php _e('Remove'); ?></th>
+						<!--<th scope="col" class="manage-column column-name" ><?php _e('Add Category'); ?></th>-->
 					</tr>
 				</tfoot>
 				<tbody id="the-list">
 			<?php
 			$index = 0;
 			if(!empty($mdocs_cats)) {
-				foreach($mdocs_cats as $key => $value) {
-					$index++;
-					?>
-						<tr>
-							<td  id="name" >
-								<input type="hidden" name="mdocs-cats[<?php echo $key; ?>][slug]" value="<?php echo $key; ?>"/>
-								<input type="text" name="mdocs-cats[<?php echo $key; ?>][name]"  value="<?php echo $value; ?>"  />
-							</td>
-							<td id="order">
-								<input type="text" name="mdocs-cats[<?php echo $key; ?>][order]"  value="<?php echo $index; ?>"  />
-							</td>
-							<td id="remove">
-								<input type="hidden" name="mdocs-cats[<?php echo $key; ?>][remove]" value="0"/>
-								<input type="button" id="mdocs-cat-remove" name="<?php echo $key; ?>" class="button button-primary" value="Remove"  />
-							</td>
-						</tr>
-					<?php
-				}
+				/*
+				array_push($mdocs_cats[0]['children'], array('slug'=>'test-fold','name'=>'SUB 1','parent'=>'documents', 'children' => array(), 'depth' => 1));
+				array_push($mdocs_cats[0]['children'], array('slug'=>'test-fold1-2','name'=>'SUB 1-2','parent'=>'documents', 'children' => array(), 'depth' => 1));
+				array_push($mdocs_cats[1]['children'], array('slug'=>'test-fold2','name'=>'SUB 2','parent'=>'new-category-2', 'children' => array(), 'depth' => 1));
+				
+				array_push($mdocs_cats[0]['children'][0]['children'], array('slug'=>'test-fold-sub1','name'=>'SUB SUB 1','parent'=>'test-folder', 'children' => array(), 'depth' => 2));
+				array_push($mdocs_cats[0]['children'][1]['children'], array('slug'=>'test-fold-sub1-2','name'=>'SUB SUB 1-2','parent'=>'test-fold1-2', 'children' => array(), 'depth' => 2));
+				array_push($mdocs_cats[1]['children'][0]['children'], array('slug'=>'test-fold2-sub2','name'=>'SUB SUB 2','parent'=>'test-folder2', 'children' => array(), 'depth' => 2));
+				
+				array_push($mdocs_cats[0]['children'][0]['children'][0]['children'], array('slug'=>'test-fold-sub3-1','name'=>'SUB SUB SUB 1','parent'=>'test-fold-sub1', 'children' => array(), 'depth' => 3));
+				array_push($mdocs_cats[0]['children'][0]['children'][0]['children'], array('slug'=>'test-fold-sub3-2','name'=>'SUB SUB SUB 2','parent'=>'test-fold-sub1', 'children' => array(), 'depth' => 3));
+				array_push($mdocs_cats[0]['children'][0]['children'][0]['children'], array('slug'=>'test-fold-sub3-3','name'=>'SUB SUB SUB 3','parent'=>'test-fold-sub1', 'children' => array(), 'depth' => 3));
+				//var_dump($mdocs_cats);
+				*/
+				$mdocs_cats = array_values($mdocs_cats);
+				mdocs_build_cat_td($mdocs_cats);
 			} else {
 				?>
 				<tr>
@@ -60,39 +60,168 @@ function mdocs_edit_cats() {
 		</form>
 	</div>
 	<?php
+	if(isset($_POST['action']) && $_POST['action'] == 'mdocs-update-cats') mdocs_update_cats();
+}
+
+function mdocs_build_cat_td($mdocs_cat) {
+	global $mdocs_input_text_bg_colors;
+	$padding = '';
+	foreach($mdocs_cat as $index => $cat) {
+		if($cat['depth'] > 0) {
+			$padding = 'style="padding-left: '.(40*$cat['depth']).'px; "';
+			
+		}
+		$color_scheme = 'style="background: '.$mdocs_input_text_bg_colors[($cat['depth'])].'"';
+		?>
+		<tr>
+			<td  id="name" <?php echo $padding; ?>>
+				<input type="hidden" name="mdocs-cats[<?php echo $cat['slug']; ?>][depth]" value="<?php echo $cat['depth']; ?>"/>
+				<input type="hidden" name="mdocs-cats[<?php echo $cat['slug']; ?>][parent]" value="<?php echo $cat['parent']; ?>"/>
+				<input type="hidden" name="mdocs-cats[<?php echo $cat['slug']; ?>][slug]" value="<?php echo $cat['slug']; ?>"/>
+				<input <?php echo $color_scheme; ?> type="text" name="mdocs-cats[<?php echo $cat['slug']; ?>][name]"  value="<?php echo $cat['name']; ?>"  />
+			</td>
+			<td id="order">
+				<input <?php echo $color_scheme; ?> type="text" name="mdocs-cats[<?php echo $cat['slug']; ?>][order]"  value="<?php echo $index+1; ?>"  />
+				
+			</td>
+			<td id="remove">
+				<input type="hidden" name="mdocs-cats[<?php echo $cat['slug']; ?>][remove]" value="0"/>
+				<?php if(count($cat['children']) == 0) { ?> 
+				<input type="button" id="mdocs-cat-remove" name="<?php echo $cat['slug']; ?>" class="button button-primary" value="Remove"  />
+				<?php } ?>
+			</td>
+			<!--
+			<td id="add-cat">
+				<input  type="button" name="mdocs-cats[<?php echo $cat['slug']; ?>][<?php echo $cat['depth']; ?>][<?php echo $cat['slug']; ?>]" class="mdocs-add-sub-cat button button-primary" value="Add Category"  />
+			</td>
+			-->
+		</tr>
+		<?php
+		$child = array_values($cat['children']);
+		if(count($child) > 0) mdocs_build_cat_td($child); 
+	}
 }
 
 function mdocs_update_cats() {
-	//$mdocs_cats = get_option('mdocs-cats');
+	$mdocs_cats = array();
 	$upload_dir = wp_upload_dir();
 	if(isset($_POST['mdocs-cats'])) {
 		$mdocs_cats_post = $_POST['mdocs-cats'];
-		foreach ($mdocs_cats_post as $key => $row) $sort_array[$key] = $row['order'];
-		array_multisort($sort_array, SORT_ASC, $mdocs_cats_post);
-		foreach($mdocs_cats_post as $key => $value) {
-			if(preg_match('/new-cat-/',$value['slug'])) {
-				$value['slug'] = preg_replace('/ /','-',strtolower($value['name']));
-				$value['slug'] = preg_replace('/[^A-Za-z0-9\-]/', '', $value['slug']);
+		//var_dump($mdocs_cats_post);	
+		$parent_id = 0;
+		$parent_ids = array();
+		$depth = 0;
+		foreach($mdocs_cats_post as $index => $cat) {
+			$test = '';
+			if(preg_match('/new-cat-/',$cat['slug'])) {
+				$cat['slug'] = preg_replace('/ /','-',strtolower($cat['name']));
+				$cat['slug'] = preg_replace('/[^A-Za-z0-9\-]/', '', $cat['slug']);
 			}
-			if($value['remove'] == 0) $mdocs_cats[$value['slug']] = $value['name'];
-			else {
-				$mdocs = get_option('mdocs-list');
-				foreach($mdocs as $k => $v) {
-					if($v['cat'] == $value['slug']) {
-						wp_delete_attachment( intval($v['id']), true );
-						wp_delete_post( intval($v['parent']), true );
-						$name = substr($v['filename'], 0, strrpos($v['filename'], '.') );
-						if(file_exists($upload_dir['basedir'].'/mdocs/'.$v['filename'])) @unlink($upload_dir['basedir'].'/mdocs/'.$v['filename']);
-						foreach($v['archived'] as $a) unlink($upload_dir['basedir'].'/mdocs/'.$a);
-						$thumbnails = glob($upload_dir['basedir'].'/mdocs/'.$name.'-150x55*');
-						foreach($thumbnails as $t) unlink($t);
-						unset($mdocs[$k]);
+			//if($cat['remove'] == 0)  {
+				$curr_depth = intval($cat['depth']);
+				$depth = intval($cat['depth']);
+				if($cat['parent'] == '')  {
+					$parent_ids = array();
+					$base_parent_id = intval($cat['order'])-1;
+					$mdocs_cats[$base_parent_id] = array('slug' => $cat['slug'], 'name' => $cat['name'], 'parent' => '', 'children' => array(), 'depth' => 0);
+					if($cat['remove'] == 1) unset($mdocs_cats[$base_parent_id]);
+				} else {
+					$child_order = intval($cat['order'])-1;
+					array_push($parent_ids,$child_order);
+					//$test = '$mdocs_cats['.$base_parent_id.']["children"]';
+					if($prev_depth > $curr_depth) {
+						for($i=$prev_depth;$i>0; $i--) unset($parent_ids[$i-1]);
+					} elseif($prev_depth == $curr_depth) unset($parent_ids[$curr_depth-1]);
+					$parent_ids = array_values($parent_ids);
+					for($i=0;$i<intval($cat['depth']); $i++) {
+						if($i == intval($cat['depth'])-1)	{
+							//$test .= '['.$child_order.']'.'["children"]';
+							$test .= $child_order;
+						} else {
+							//$test .= '['.$parent_ids[$i].']'.'["children"]';
+							$test .= $parent_ids[$i].',';
+						}
+					}		
+					$prev_depth =$curr_depth;
+					//$test .= ' ==> '.$cat['name'];	
+					$parent_id = $child_order;
+					//var_dump($parent_ids);
+					$output = explode(',',$test);
+					//var_dump($output);
+					$base_array = array();
+					foreach($output as $index => $op) {
+						if($index == 0) $base_id = $op;
+						elseif($index == 1) $id_1 = $op;
+						elseif($index == 2) $id_2 = $op;
+						elseif($index == 3) $id_3 = $op;
+						elseif($index == 4) $id_4 = $op;
+					}
+					if($depth == 1) {
+						$mdocs_cats[$base_parent_id]['children'][$base_id] = array('slug' => $cat['slug'], 'name' => $cat['name'], 'parent' => $cat['parent'], 'children' => array(), 'depth' => 1);
+						if($cat['remove'] == 1) unset($mdocs_cats[$base_parent_id]['children'][count($mdocs_cats[$base_parent_id]['children'])-1]);
+						ksort($mdocs_cats[$base_parent_id]['children']);
+					} elseif($depth == 2) {
+						$mdocs_cats[$base_parent_id]['children'][$base_id]['children'][$id_1] = array('slug' => $cat['slug'], 'name' => $cat['name'], 'parent' => $cat['parent'], 'children' => array(), 'depth' => 2);
+						if($cat['remove'] == 1) unset($mdocs_cats[$base_parent_id]['children'][$base_id]['children'][count($mdocs_cats[$base_parent_id]['children'][$base_id]['children'])-1]);
+						ksort($mdocs_cats[$base_parent_id]['children'][$base_id]['children']);
+						//$mdocs_cats[$base_parent_id]['children'][$base_id]['children'] = array_values($mdocs_cats[$base_parent_id]['children'][$base_id]['children']);
+					} elseif($depth == 3) {
+						$mdocs_cats[$base_parent_id]['children'][$base_id]['children'][$id_1]['children'][$id_2] = array('slug' => $cat['slug'], 'name' => $cat['name'], 'parent' => $cat['parent'], 'children' => array(), 'depth' =>3);
+						if($cat['remove'] == 1) unset($mdocs_cats[$base_parent_id]['children'][$base_id]['children'][$id_1]['children'][count($mdocs_cats[$base_parent_id]['children'][$base_id]['children'][$id_1]['children'])-1]);
+						ksort($mdocs_cats[$base_parent_id]['children'][$base_id]['children'][$id_1]['children']);
+					} elseif($depth == 4) {
+						$mdocs_cats[$base_parent_id]['children'][$base_id]['children'][$id_1]['children'][$id_2]['children'][$id_3] = array('slug' => $cat['slug'], 'name' => $cat['name'], 'parent' => $cat['parent'], 'children' => array(), 'depth' => 4);
+						ksort($mdocs_cats[$base_parent_id]['children'][$base_id]['children'][$id_1]['children'][$id_2]['children']);
 					}
 				}
-				update_option('mdocs-list',$mdocs);
-			}
+				$parent_slug = $cat['slug'];
+				if($cat['remove'] == 1) mdocs_cleanup_cats($cat);
+			//}
 		}
+		//$mdocs_cats = mdocs_cats_loop($mdocs_cats);
+		//var_dump($test);
+		//var_dump($mdocs_cats);
+		
+		ksort($mdocs_cats);
 		update_option('mdocs-cats',$mdocs_cats);
 	}
+}
+
+function mdocs_cats_loop($the_cats) {
+	foreach($the_cats as $key => $cat) {
+		if(count($cat['children']) > 0) {
+			//ksort($the_cats[$key]['children']);
+			$the_cats[$key]['children'] = array_values($the_cats[$key]['children']);
+			mdocs_cats_loop($the_cats[$key]['children']);
+		}
+	}
+	return $the_cats;
+}
+
+function mdocs_cleanup_cats($value) {
+	$upload_dir = wp_upload_dir();
+	$mdocs = get_option('mdocs-list');
+	$mdocs_cats = get_option('mdocs-cats');
+	foreach($mdocs as $k => $v) {
+		if($v['cat'] == $value['slug']) {
+			wp_delete_attachment( intval($v['id']), true );
+			wp_delete_post( intval($v['parent']), true );
+			$name = substr($v['filename'], 0, strrpos($v['filename'], '.') );
+			if(file_exists($upload_dir['basedir'].'/mdocs/'.$v['filename'])) @unlink($upload_dir['basedir'].'/mdocs/'.$v['filename']);
+			foreach($v['archived'] as $a) @unlink($upload_dir['basedir'].'/mdocs/'.$a);
+			$thumbnails = glob($upload_dir['basedir'].'/mdocs/'.$name.'-150x55*');
+			foreach($thumbnails as $t) unlink($t);
+			unset($mdocs[$k]);
+		}
+	}
+	if(isset($value['children'])) {
+		if(count($value['children']) > 0) {
+			foreach($value['children'] as $key) {
+				mdocs_cleanup_cats($key);	
+			}
+		}
+	}
+
+	update_option('mdocs-list',$mdocs);
 }
 ?>
