fba47613f43e0cf38cc7d67a1fa0eeb7c183e4cf
bhaldie <bhaldie@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-20 15:26:37 +0000

    [ Release ][ Version 2.5.1.1 ] * *Hot-Fix* - Added the style.css file to the admin page. Now the page will display the correct style.
    git-svn-id: https://plugins.svn.wordpress.org/memphis-documents-library/tags/2.5.1.1@935759 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- mdocs-categories.php
+++ mdocs-categories.php
@@ -1,18 +1,22 @@
 <?php
 function mdocs_edit_cats() {
 	$mdocs_cats = get_option('mdocs-cats');
-	//var_dump($mdocs_cats);
+	//var_dump($mdocs_cats[0]);
+	//var_dump('Number of Categories: '.get_option('mdocs-num-cats'));
+	//var_dump($mdocs_cats[1]['children']);
 	?>
 	<div class="mdocs-ds-container">
-		<h2>Category Editor <a href="" id="mdocs-add-cat" class="mdocs-grey-btn">Add Category</a></h2>
+		<h2>Category Editor <a href="" id="mdocs-add-cat" class="mdocs-grey-btn"><?php _e('Add Main Category'); ?></a></h2>
 		<form  id="mdocs-cats" method="post" action="admin.php?page=memphis-documents.php&cat=cats" >
 			<input type="hidden" value="mdocs-update-cats" name="action"/>
+			<input type="hidden" name="mdocs-update-cat-index" value="0"/>
 			<table class="wp-list-table widefat plugins">
 				<thead>
 					<tr>
 						<th scope="col" class="manage-column column-name" ><?php _e('Category'); ?></th>
 						<th scope="col"  class="manage-column column-name" ><?php _e('Order'); ?></th>
 						<th scope="col"  class="manage-column column-name" ><?php _e('Remove'); ?></th>
+						<th scope="col" class="manage-column column-name" ><?php _e('Add Category'); ?></th>
 					</tr>
 				</thead>
 				<tfoot>
@@ -20,30 +24,15 @@ function mdocs_edit_cats() {
 						<th scope="col" class="manage-column column-name" ><?php _e('Category'); ?></th>
 						<th scope="col" class="manage-column column-name" ><?php _e('Order'); ?></th>
 						<th scope="col" class="manage-column column-name" ><?php _e('Remove'); ?></th>
+						<th scope="col" class="manage-column column-name" ><?php _e('Add Category'); ?></th>
 					</tr>
 				</tfoot>
 				<tbody id="the-list">
 			<?php
 			$index = 0;
 			if(!empty($mdocs_cats)) {
-				foreach($mdocs_cats as $key => $value) {
-					$index++;
-					?>
-						<tr>
-							<td  id="name" >
-								<input type="hidden" name="mdocs-cats[<?php echo $key; ?>][slug]" value="<?php echo $key; ?>"/>
-								<input type="text" name="mdocs-cats[<?php echo $key; ?>][name]"  value="<?php echo $value; ?>"  />
-							</td>
-							<td id="order">
-								<input type="text" name="mdocs-cats[<?php echo $key; ?>][order]"  value="<?php echo $index; ?>"  />
-							</td>
-							<td id="remove">
-								<input type="hidden" name="mdocs-cats[<?php echo $key; ?>][remove]" value="0"/>
-								<input type="button" id="mdocs-cat-remove" name="<?php echo $key; ?>" class="button button-primary" value="Remove"  />
-							</td>
-						</tr>
-					<?php
-				}
+				$mdocs_cats = array_values($mdocs_cats);
+				mdocs_build_cat_td($mdocs_cats);
 			} else {
 				?>
 				<tr>
@@ -60,39 +49,139 @@ function mdocs_edit_cats() {
 		</form>
 	</div>
 	<?php
+	if(isset($_POST['action']) && $_POST['action'] == 'mdocs-update-cats') mdocs_update_cats();
+}
+function mdocs_build_cat_td($mdocs_cat,$parent_index=0) {
+	global $mdocs_input_text_bg_colors;
+	$padding = '';
+	foreach($mdocs_cat as $index => $cat) {
+		if($cat['depth'] > 0) {
+			$padding = 'style="padding-left: '.(40*$cat['depth']).'px; "';
+			
+		}
+		$color_scheme = 'style="background: '.$mdocs_input_text_bg_colors[($cat['depth'])].'"';
+		?>
+		<tr>
+			<td  id="name" <?php echo $padding; ?>>
+				<input type="hidden" name="mdocs-cats[<?php echo $cat['slug']; ?>][index]" value="<?php echo $index; ?>"/>
+				<input type="hidden" name="mdocs-cats[<?php echo $cat['slug']; ?>][parent_index]" value="<?php echo $parent_index; ?>"/>
+				<input type="hidden" name="mdocs-cats[<?php echo $cat['slug']; ?>][num_children]" value="<?php echo count($cat['children']); ?>"/>
+				<input type="hidden" name="mdocs-cats[<?php echo $cat['slug']; ?>][depth]" value="<?php echo $cat['depth']; ?>"/>
+				<input type="hidden" name="mdocs-cats[<?php echo $cat['slug']; ?>][parent]" value="<?php echo $cat['parent']; ?>"/>
+				<input type="hidden" name="mdocs-cats[<?php echo $cat['slug']; ?>][slug]" value="<?php echo $cat['slug']; ?>"/>
+				<input <?php echo $color_scheme; ?> type="text" name="mdocs-cats[<?php echo $cat['slug']; ?>][name]"  value="<?php echo $cat['name']; ?>" />
+			</td>
+			<td id="order">
+				<input <?php echo $color_scheme; ?> type="text" name="mdocs-cats[<?php echo $cat['slug']; ?>][order]"  value="<?php echo $index+1; ?>" <?php if($cat['parent'] != '') echo 'disabled'; ?> title="Sorry this functionality is disabled"/>
+				
+			</td>
+			<td id="remove">
+				<input type="hidden" name="mdocs-cats[<?php echo $cat['slug']; ?>][remove]" value="0"/>
+				<?php if(count($cat['children']) == 0) { ?> 
+				<input type="button" id="mdocs-cat-remove" name="<?php echo $cat['slug']; ?>" class="button button-primary" value="Remove"  />
+				<?php } ?>
+			</td>
+			<td id="add-cat">
+				<input  type="button" class="mdocs-add-sub-cat button button-primary" value="Add Category" onclick="mdocs_add_sub_cat( '<?php echo intval(get_option('mdocs-num-cats')); ?>', '<?php echo $cat['slug']; ?>','<?php echo $cat['depth']; ?>', this);"  />
+			</td>
+		</tr>
+		<?php
+		$child = array_values($cat['children']);
+		if(count($child) > 0) mdocs_build_cat_td($child,$index);
+	}
 }
 
 function mdocs_update_cats() {
-	//$mdocs_cats = get_option('mdocs-cats');
+	$mdocs_cats = array();
 	$upload_dir = wp_upload_dir();
+	if(isset($_POST['mdocs-update-cat-index'])) mdocs_update_num_cats(intval($_POST['mdocs-update-cat-index']));
 	if(isset($_POST['mdocs-cats'])) {
 		$mdocs_cats_post = $_POST['mdocs-cats'];
-		foreach ($mdocs_cats_post as $key => $row) $sort_array[$key] = $row['order'];
-		array_multisort($sort_array, SORT_ASC, $mdocs_cats_post);
-		foreach($mdocs_cats_post as $key => $value) {
-			if(preg_match('/new-cat-/',$value['slug'])) {
-				$value['slug'] = preg_replace('/ /','-',strtolower($value['name']));
-				$value['slug'] = preg_replace('/[^A-Za-z0-9\-]/', '', $value['slug']);
-			}
-			if($value['remove'] == 0) $mdocs_cats[$value['slug']] = $value['name'];
-			else {
-				$mdocs = get_option('mdocs-list');
-				foreach($mdocs as $k => $v) {
-					if($v['cat'] == $value['slug']) {
-						wp_delete_attachment( intval($v['id']), true );
-						wp_delete_post( intval($v['parent']), true );
-						$name = substr($v['filename'], 0, strrpos($v['filename'], '.') );
-						if(file_exists($upload_dir['basedir'].'/mdocs/'.$v['filename'])) @unlink($upload_dir['basedir'].'/mdocs/'.$v['filename']);
-						foreach($v['archived'] as $a) unlink($upload_dir['basedir'].'/mdocs/'.$a);
-						$thumbnails = glob($upload_dir['basedir'].'/mdocs/'.$name.'-150x55*');
-						foreach($thumbnails as $t) unlink($t);
-						unset($mdocs[$k]);
-					}
+		$parent_id = 0;
+		$parent_ids = array();
+		$depth = 0;
+		$prev_depth = 0;
+		foreach($mdocs_cats_post as $index => $cat) {
+			$cat['index'] = intval($cat['index']);
+			$cat['parent_index'] = intval($cat['parent_index']);
+			$cat['depth'] = intval($cat['depth']);
+			//$cat['order'] = intval($cat['order']);
+			$test = '';
+			$curr_depth = intval($cat['depth']);
+			$depth = intval($cat['depth']);
+			if($cat['parent'] == '')  {
+				$parent_ids = array();
+				$base_parent_id = intval($cat['order'])-1;
+				$mdocs_cats[$base_parent_id] = array('base_parent'=>'','index' => $cat['index'], 'parent_index'=>$cat['parent_index'], 'slug' => $cat['slug'], 'name' => $cat['name'], 'parent' => '', 'children' => array(), 'depth' => 0);
+				if($cat['remove'] == 1) unset($mdocs_cats[$base_parent_id]);
+			} else {
+				if($depth == 1) {
+					$mdocs_cats[$base_parent_id]['children'][$cat['index']] = array('base_parent'=>$base_parent_id,'index' => $cat['index'], 'parent_index'=>$cat['parent_index'], 'slug' => $cat['slug'], 'name' => $cat['name'], 'parent' => $cat['parent'], 'children' => array(), 'depth' => 1);
+					if($cat['remove'] == 1) unset($mdocs_cats[$base_parent_id]['children'][$cat['index']]);
+					ksort($mdocs_cats[$base_parent_id]['children']);
+					$index1 = $cat['index'];
+				} elseif($depth == 2) {
+					$mdocs_cats[$base_parent_id]['children'][$cat['parent_index']]['children'][$cat['index']] = array('base_parent'=>$base_parent_id,'index' => $cat['index'], 'parent_index'=>$cat['parent_index'],'slug' => $cat['slug'], 'name' => $cat['name'], 'parent' => $cat['parent'], 'children' => array(), 'depth' => 2);
+					if($cat['remove'] == 1) unset($mdocs_cats[$base_parent_id]['children'][$cat['parent_index']]['children'][$cat['index']]);
+					ksort($mdocs_cats[$base_parent_id]['children'][$cat['parent_index']]['children']);
+					$index2 = $cat['index'];
 				}
-				update_option('mdocs-list',$mdocs);
+				/* Work in Progress
+				} elseif($depth == 3) {
+					$mdocs_cats[$base_parent_id]['children'][$index1]['children'][$cat['parent_index']]['children'][$cat['index']] = array('slug' => $cat['slug'], 'name' => $cat['name'], 'parent' => $cat['parent'], 'children' => array(), 'depth' =>3);
+					if($cat['remove'] == 1) unset($mdocs_cats[$base_parent_id]['children'][$index1]['children'][$cat['parent_index']]['children'][$cat['index']]);
+					ksort($mdocs_cats[$base_parent_id]['children'][$index1]['children'][$cat['parent_index']]['children']);
+				} elseif($depth == 4) {
+					$mdocs_cats[$base_parent_id]['children'][$base_id]['children'][$id_1]['children'][$id_2]['children'][$id_3] = array('slug' => $cat['slug'], 'name' => $cat['name'], 'parent' => $cat['parent'], 'children' => array(), 'depth' => 4);
+					ksort($mdocs_cats[$base_parent_id]['children'][$base_id]['children'][$id_1]['children'][$id_2]['children']);
+				}
+				*/
 			}
+			$parent_slug = $cat['slug'];
+			if($cat['remove'] == 1) mdocs_cleanup_cats($cat);
 		}
+		
+		ksort($mdocs_cats);
+		$mdocs_cats = array_values($mdocs_cats);
 		update_option('mdocs-cats',$mdocs_cats);
 	}
+}
+
+function mdocs_cats_loop($the_cats) {
+	foreach($the_cats as $key => $cat) {
+		if(count($cat['children']) > 0) {
+			//ksort($the_cats[$key]['children']);
+			$the_cats[$key]['children'] = array_values($the_cats[$key]['children']);
+			mdocs_cats_loop($the_cats[$key]['children']);
+		}
+	}
+	return $the_cats;
+}
+
+function mdocs_cleanup_cats($value) {
+	$upload_dir = wp_upload_dir();
+	$mdocs = get_option('mdocs-list');
+	$mdocs_cats = get_option('mdocs-cats');
+	foreach($mdocs as $k => $v) {
+		if($v['cat'] == $value['slug']) {
+			wp_delete_attachment( intval($v['id']), true );
+			wp_delete_post( intval($v['parent']), true );
+			$name = substr($v['filename'], 0, strrpos($v['filename'], '.') );
+			if(file_exists($upload_dir['basedir'].'/mdocs/'.$v['filename'])) @unlink($upload_dir['basedir'].'/mdocs/'.$v['filename']);
+			foreach($v['archived'] as $a) @unlink($upload_dir['basedir'].'/mdocs/'.$a);
+			$thumbnails = glob($upload_dir['basedir'].'/mdocs/'.$name.'-150x55*');
+			foreach($thumbnails as $t) unlink($t);
+			unset($mdocs[$k]);
+		}
+	}
+	if(isset($value['children'])) {
+		if(count($value['children']) > 0) {
+			foreach($value['children'] as $key) {
+				mdocs_cleanup_cats($key);	
+			}
+		}
+	}
+
+	update_option('mdocs-list',$mdocs);
 }
 ?>
---
+++ mdocs-shortcodes.php
@@ -0,0 +1,32 @@
+<?php
+function mdocs_shortcodes($current_cat) {
+	?>
+	<br>
+	<div class="updated">
+		<h3>Short Codes</h3>
+		<table class="mdocs-shortcode-table" >
+			<tr>
+				<th><?php _e('Short Codes');?></th>
+				<th><?php _e('Description');?></th>
+			</tr>
+			<tr>
+				<td>[mdocs]</td>
+				<td><?php _e('Adds the default Memphis Documents Library file list to any page, post or widget.');?></td>
+			</tr>
+			<tr>
+				<td>[mdocs cat="<?php _e('The Category Name');?>"]</td>
+				<td><?php _e('Adds files from  a specific category of the Memphis Documents Library on any page, post or widget.');?></td>
+			</tr>
+			<tr>
+				<td>[mdocs cat="All Files"]</td>
+				<td><?php _e('Adds a list of all files of the Memphis Documents Library on any page, post or widget.');?></td>
+			</tr>
+			<tr>
+				<td>[mdocs header="<?php _e('This text will show up above the documents list.'); ?>"]</td>
+				<td><?php _e('Adds a header to the Memphis Documents LIbrary on ay page, post or widget.');?></td>
+			</tr>
+		</table>
+	</div>
+	<?php
+}
+?>
