6f1d7e78db88b0f6dc20e233422da3169b1952d0
bhaldie <bhaldie@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-06 13:41:30 +0000

    [ Release ][ Version 2.6.2 ] * *Bug* - Fixed a bug when creating categories a null category would be created that could not be delete.
    git-svn-id: https://plugins.svn.wordpress.org/memphis-documents-library/tags/2.6.2@961076 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- memphis-documents.php
+++ memphis-documents.php
@@ -1,15 +1,14 @@
 <?php
 /*
- 
 Plugin Name: Memphis Documents Library
-Plugin URI: http://
+Plugin URI: http://www.kingofnothing.net/memphis-documents-library/
 Description: A documents repository for WordPress. 
 Author: Ian Howatson
-Version: 1.2.6
+Version: 2.6.2
 Author URI: http://www.kingofnothing.net/
-Date: 09/06/2013
+Date: 08/06/2014
 
-Copyright 2013 Ian Howatson  (email : ian.howatson@kingofnothing.net)
+Copyright 2014 Ian Howatson  (email : ian@howatson.net)
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License, version 2, as 
@@ -26,7 +25,10 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 include 'mdocs-settings.php';
 include 'mdocs-functions.php';
-include 'mdocs-page.php';
+include 'mdocs-the-list.php';
+include 'mdocs-file-info-large.php';
+include 'mdocs-file-info-small.php';
+include 'mdocs-social.php';
 include 'mdocs-dashboard.php';
 include 'mdocs-import.php';
 include 'mdocs-export.php';
@@ -34,10 +36,25 @@ include 'mdocs-upload.php';
 include 'mdocs-categories.php';
 include 'mdocs-downloads.php';
 include 'mdocs-versions.php';
+include 'mdocs-batch-upload.php';
+include 'mdocs-settings-page.php';
+include 'mdocs-shortcodes.php';
 include 'mdocs-localization.php';
+include 'mdocs-browser-compatibility.php';
+include 'mdocs-widgets.php';
 mdocs_nonce();
+if(!headers_sent() && stripos($_SERVER['REQUEST_URI'], '/feed') === false) add_action('send_headers', 'mdocs_send_headers');
+elseif (!is_numeric(stripos($_SERVER['REQUEST_URI'], '/feed'))) {
+	$message = sprintf('Premature output is preventing Memphis Documents Library from working properly. Outpust has started in %s on line %d.', $file, $line);
+	echo '<div style="border: 1em solid red; background: #fff; color: #f00; margin:2em; padding: 1em;">', htmlspecialchars($message), '</div>';
+	trigger_error($message);
+	die();	
+}
+if ( is_admin()) add_action('admin_init', 'mdocs_send_headers_dashboard');
+add_action('init', 'mdocs_localize');
 add_action('admin_menu', 'mdocs_dashboard_menu');
 add_action( 'wp_enqueue_scripts', 'mdocs_script' );
-add_action('wp_footer', 'mdocs_social_scripts');
-add_action('admin_footer', 'mdocs_social_scripts');
+add_action('wp_footer', 'mdocs_document_ready_wp');
+add_action('admin_footer', 'mdocs_document_ready_admin');
+add_action( 'widgets_init', 'mdocs_widgets' );
 ?>
